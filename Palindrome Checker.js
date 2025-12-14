73142505f65da902dccf96656ad73f65f3446b9e
EGranier25 <emmagranier890@gmail.com>
2025-12-13 19:24:01 -0600

    Update Palindrome Checker.js
    added comments

--- Palindrome Checker.js
+++ Palindrome Checker.js
@@ -1,19 +1,19 @@
-function strings()
+function strings() // Palindrome Checker
 {
-    var string1 = document.getElementById("str1").value;
+    var string1 = document.getElementById("str1").value; // Get the input string from the user
     
-    var newString ="";
+    var newString =""; // Initialize an empty string to store the reversed string
         var i;
-        for (i = string1.length - 1; i >= 0; i--)
+        for (i = string1.length - 1; i >= 0; i--) // Loop through the string in reverse order
         {
-            newString += string1[i];
+            newString += string1[i]; 
         }
 
-        if(newString === string1)   
+        if(newString === string1)  // Check if the reversed string is the same as the original string
         {
             document.getElementById("result").innerHTML ="This is a palindrome. Congrats!";
         }   
-        else
+        else // If not a palindrome
         {
             document.getElementById("result").innerHTML ="This is not a palindrome. Try again!";
         }
